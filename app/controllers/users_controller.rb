42e9c97961a06df37f6d60c31548a0393b4d811e
AKod13 <akodzoman13@gmail.com>
2023-05-11 14:41:44 +0200

    Cleaning the layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "User account information was updated succesfully"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
